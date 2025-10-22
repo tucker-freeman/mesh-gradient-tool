5ec2b5cfe17274d1d8d9bddaa8359446bb9bdbbf
tucker-freeman <tucker@breadproject.com>
2025-10-22 07:31:19 -0500

    Update GradientCanvas.jsxUpdate GradientCanvas.jsx to improved gradient

--- src/GradientCanvas.jsx
+++ src/GradientCanvas.jsx
@@ -1,26 +1,30 @@
 import React, { useRef } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
-import { MeshDistortMaterial, Sphere } from '@react-three/drei'
+import { Sphere, MeshDistortMaterial, OrbitControls } from '@react-three/drei'
 
 export default function GradientCanvas() {
   const sphere = useRef()
 
-  useFrame((state, delta) => {
-    sphere.current.rotation.y += delta * 0.1
-    sphere.current.rotation.x += delta * 0.05
+  useFrame((_, delta) => {
+    sphere.current.rotation.y += delta * 0.25
   })
 
   return (
-    <Canvas camera={{ position: [0, 0, 4], fov: 50 }}>
-      <ambientLight intensity={1.5} />
-      <Sphere args={[1.2, 128, 128]} ref={sphere}>
+    <Canvas
+      camera={{ position: [0, 0, 3], fov: 50 }}
+      style={{ background: 'radial-gradient(circle at 30% 30%, #b38dff, #6563ac)' }}
+    >
+      <ambientLight intensity={1.2} />
+      <directionalLight position={[2, 2, 3]} intensity={1.5} />
+      <Sphere args={[1, 64, 64]} ref={sphere}>
         <MeshDistortMaterial
-          color="#b38dff"
+          color="#ffffff"
           distort={0.4}
           speed={0.8}
           roughness={0.1}
         />
       </Sphere>
+      <OrbitControls enableZoom={false} />
     </Canvas>
   )
 }
